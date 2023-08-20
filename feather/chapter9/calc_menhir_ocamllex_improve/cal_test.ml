ffcd33fc04d725a8c2846be34dfe3ce131e9b21d
Yixian <2789582528@qq.com>
2023-08-20 22:02:57 +0800

    [ADD] an improved calculator supporting float,Div, MINUS, NEG

---
+++ feather/chapter9/calc_menhir_ocamllex_improve/cal_test.ml
@@ -0,0 +1,31 @@
+open OUnit2
+open Main
+
+let make_i n i s =
+  n >:: (fun _ -> assert_equal (string_of_int i) (interp s)~printer:(fun x -> x))
+
+let make_f n f s =
+  n >:: (fun _ -> assert_equal (string_of_float f) (interp s)~printer:(fun x -> x))
+let tests = [
+  make_i "int" 22 "22";
+  make_i "int" (-22) "-22";
+  make_i "add" 22 "11+11";
+  make_i "mult" 22 "2*11";
+  make_i "mult of mult" 40 "2*2*10";
+  make_i "mult on right of add" 22 "2+2*10";
+  make_i "mult on left of add" 14 "2*2+10";
+  make_i "nested add" 22 "(10 + 1 ) + ( 5 + 6)";
+  make_i "nested add and mult" 121 "(10 + 1 ) * ( 5 + 6)";
+  make_i "minus" 5 "10 - 5";
+  make_i "div" 2 "10 / 5";
+  make_i "complex" 0 "1+(10 - 3)*2/7-(5*4/10+2-1)";
+  make_f "float" 22.0 "22.";
+  make_f "float" 22.0 "22.0";
+  make_f "add" 22.0 "11.0 +11.";
+  make_f "mult" 22.0 "11.0 * 2";
+  make_f "minus" 10.0 "15 - 5.0";
+  make_f "div"   2.5 "5 / 2";
+  make_f "nested" 0.0 "10*(2/(5+3)*(7-4.5))";
+]
+
+let _ = run_test_tt_main ("suite" >::: tests)
---
+++ feather/chapter9/calc_menhir_ocamllex_improve/parser.mly
@@ -0,0 +1,33 @@
+%token <int> INT
+%token <float> FLOAT
+%token TIMES
+%token PLUS
+%token DIV
+%token MINUS
+%token LPAREN
+%token RPAREN
+%token EOF
+
+// %right PLUS
+%left PLUS MINUS /* lowest precedence */
+%left TIMES DIV /* medium precedence */
+%nonassoc UMINUS /* highest precedence*/
+
+%start <Ast.expr> prog
+%type <Ast.expr> expr
+%%
+
+prog :
+	| e = expr; EOF { e }
+	;
+
+expr:
+	| f = FLOAT { Float f }
+	| i = INT { Int i }
+	| e1 = expr; TIMES; e2 = expr  { Binop (Mul, e1, e2) }
+	| e1 = expr; PLUS; e2 = expr  { Binop (Add, e1, e2) }
+	| e1 = expr; MINUS; e2 = expr  { Binop (MINUS, e1, e2) }
+	| e1 = expr; DIV; e2 = expr  { Binop (DIV, e1, e2) }
+	| LPAREN; e = expr; RPAREN { e }
+	| MINUS INT %prec UMINUS { Int (-$2) }
+	;
